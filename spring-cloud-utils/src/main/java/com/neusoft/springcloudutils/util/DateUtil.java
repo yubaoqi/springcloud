ca4b60b74329d8d6a4abc38f0942db3188e8d516
yubaoqi <1657899428@qq.com>
2019-03-21 09:40:02 +0800

    新增一个语句

--- spring-cloud-utils/src/main/java/com/neusoft/springcloudutils/util/DateUtil.java
+++ spring-cloud-utils/src/main/java/com/neusoft/springcloudutils/util/DateUtil.java
@@ -7,6 +7,7 @@ public class DateUtil {
     public static String getCurrentDate(){
         Date date = new Date();
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println("test");
         return "当前时间 "+format.format(date);
     }
 }
